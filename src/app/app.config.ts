b4d7cf7f839e70cce40469d2f219325b05a46578
M4ul4 <maula.haqq@unicornintertranz.com>
2026-08-11 14:45:31 +0700

    feat: add PrimeIcons and ngx-translate dependencies, update styles and app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,16 +6,30 @@ import { provideClientHydration } from '@angular/platform-browser';
 
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
-import { environment } from './environments/environment';
+import { environment } from '../environments/environment';
+import { FilterMatchMode } from 'primeng/api';
+// import { provideTranslateService } from '@ngx-translate/core';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(),
+    provideHttpClient(),
+    // provideTranslateService({
+    //   // loader: provideTranslateHttpLoader({
+    //   //   prefix: '/assets/i18n/',
+    //   //   suffix: '.json',
+    //   // }),
+    //   fallbackLang: 'en',
+    //   lang: 'en',
+    // }),
     providePrimeNG({
       license: environment.primeNG,
       ripple: true,
+      overlayAppendTo: 'body',
+
       theme: {
         preset: Aura,
         options: {
@@ -24,6 +38,41 @@ export const appConfig: ApplicationConfig = {
           cssLayer: true,
           cssVariables: true,
         },
+      },
+      zIndex: {
+        modal: 1100,
+        overlay: 1000,
+        menu: 1000,
+        tooltip: 1100,
+      },
+      filterMatchModeOptions: {
+        text: [
+          FilterMatchMode.STARTS_WITH,
+          FilterMatchMode.CONTAINS,
+          FilterMatchMode.NOT_CONTAINS,
+          FilterMatchMode.ENDS_WITH,
+          FilterMatchMode.EQUALS,
+          FilterMatchMode.NOT_EQUALS,
+        ],
+        numeric: [
+          FilterMatchMode.EQUALS,
+          FilterMatchMode.NOT_EQUALS,
+          FilterMatchMode.LESS_THAN,
+          FilterMatchMode.LESS_THAN_OR_EQUAL_TO,
+          FilterMatchMode.GREATER_THAN,
+          FilterMatchMode.GREATER_THAN_OR_EQUAL_TO,
+        ],
+        date: [
+          FilterMatchMode.DATE_IS,
+          FilterMatchMode.DATE_IS_NOT,
+          FilterMatchMode.DATE_BEFORE,
+          FilterMatchMode.DATE_AFTER,
+        ],
+      },
+      translation: {
+        accept: 'Terima',
+        reject: 'Tolak',
+        //translations
       },
     }),
   ],
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidebars } from './component/misc/sidebars/sidebars';
+// import { PrimeNG } from 'primeng/config';
+// import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Sidebars],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('submission-halte-addinson');
+
+  // constructor(
+  //   private config: PrimeNG,
+  //   private translateService: TranslateService,
+  // ) {}
+  // ngOnInit() {
+  //   this.translateService.setDefaultLang('en');
+  // }
+  // translate(lang: string) {
+  //   this.translateService.use(lang);
+  //   this.translateService.get('primeng').subscribe((res) => this.primeng.setTranslation(res));
+  // }
 }
