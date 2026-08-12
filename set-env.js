8953e8219fe8102346c58578f0c9edc02b89c234
Maula Syabil Haqq <hanya.haqq@gmail.com>
2026-08-13 02:18:39 +0700

    fix: fix environment setup to use synchronous file writing and correct target paths

--- set-env.js
+++ set-env.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
-const { argv } = require('yargs');
 
-const targetFolder = './src/environments';
-const targetPath = `${targetFolder}/environment.production.ts`;
+const targetFolder = './environments';
+const envProdPath = `${targetFolder}/environment.production.ts`;
+const envDefaultPath = `${targetFolder}/environment.ts`;
 
 if (!fs.existsSync(targetFolder)) {
   console.log(`Folder ${targetFolder} tidak ditemukan. Membuat folder baru...`);
@@ -12,19 +12,20 @@ if (!fs.existsSync(targetFolder)) {
 const envConfigFile = `
 export const environment = {
   production: true,
-  primeng:
-    '${process.env.PRIMENG}',
+  primeng: '${process.env.PRIMENG}',
   domain: '${process.env.AUTH0_DOMAIN}',
   clientId: '${process.env.AUTH0_CLIENT_ID}',
   clientSecret: '${process.env.AUTH0_CLIENT_SECRET}',
 };
 `;
 
-fs.writeFile(targetPath, envConfigFile, function (err) {
-  if (err) {
-    console.error('Gagal membuat file environment:', err);
-    throw err;
-  } else {
-    console.log(`Berhasil membuat file environment di ${targetPath}`);
-  }
-});
+try {
+  fs.writeFileSync(envProdPath, envConfigFile);
+  console.log(`Berhasil membuat: ${envProdPath}`);
+
+  fs.writeFileSync(envDefaultPath, envConfigFile);
+  console.log(`Berhasil membuat: ${envDefaultPath}`);
+} catch (err) {
+  console.error('Gagal menulis file environment:', err);
+  process.exit(1);
+}
